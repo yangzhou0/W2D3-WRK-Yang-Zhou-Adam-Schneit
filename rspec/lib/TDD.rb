fc5e6eb238761dd7e46f7b0502f037bc4ed857f6
Yang & Adam <yzhou3991@gmail.com>
2018-01-24 14:07:50 -0500

    add two_sum method

--- rspec/lib/TDD.rb
+++ rspec/lib/TDD.rb
@@ -6,4 +6,14 @@ class Array
     end
     result
   end
+  
+  def two_sum
+    arr = []
+    (0..self.length-2).each do |i|
+      (i+1..self.length-1).each do |j|
+        arr << [i,j] if self[i] + self[j] == 0
+      end
+    end
+    arr
+  end
 end
--- rspec/spec/TDD_spec.rb
+++ rspec/spec/TDD_spec.rb
@@ -2,10 +2,6 @@ require 'rspec'
 require 'TDD'
 
 describe "#my_uniq" do
-  # it "should take in an Array" 
-  # 
-  #   expect([1,2,1,3,3].my_uniq).to eq ([1,2,3]) 
-  # end
   let(:arr){[1, 2, 1, 3, 3]} 
   
   it 'should not use built-in uniq' do
@@ -21,5 +17,19 @@ describe "#my_uniq" do
     expect{arr.my_uniq}.to_not change{arr}
   
   end
-  
 end
+  
+describe "#two_sum" do
+   let(:arr){[-1,0,2,-2,1]}
+   
+   it "puts smaller index first" do
+     result = arr.two_sum
+     new_array = [result[0].first, result[1].first]
+     expect(new_array).to eq(new_array.sort)
+   end
+   
+   it "pairs in the result add up to 0" do
+     expect(arr.two_sum).to eq([[0,4], [2,3]])
+   end
+end
+  
